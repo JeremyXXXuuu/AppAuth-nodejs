1a800a186549c1b526ee9a3c0aff5cc4056c9a12
JJeremyXU <jeremy.xu@orosound.com>
2023-05-03 17:16:26 +0200

    add token request flow tests

--- src/__tests__/auth.test.ts
+++ src/__tests__/auth.test.ts
@@ -1,13 +1,15 @@
 import { Auth } from "../Auth/auth";
+import { StringMap } from "../Auth/types";
 
 describe('Auth tests', () => {
     const openIdConnectUrl = "https://staging.auth.orosound.com";
     const clientId = "foo";
     const redirectUri = "http://127.0.0.1:8000";
-    const scope = "openid profile email";
+    const scope = "openid profile email offline_access";
     const responseType = "code";
+    const extras: StringMap = { prompt: "consent", access_type: "offline" };
+    const authFlow = new Auth(openIdConnectUrl, clientId, redirectUri, scope, responseType, extras);
 
-    const authFlow = new Auth(openIdConnectUrl, clientId, redirectUri, scope, responseType);
     it('Initialization should work', () => {
         expect(authFlow).toBeTruthy();
         expect(authFlow.openIdConnectUrl).toBe(openIdConnectUrl);
@@ -25,11 +27,46 @@ describe('Auth tests', () => {
         expect(authFlow.configuration.tokenEndpoint).toBeTruthy();
     });
 
-    it('makeAuthorizationRequest should work', async () => {
-      await authFlow.authRequest();
-      expect(authFlow.authorizationResponse).toBeTruthy();
-      expect(authFlow.authorizationResponse.code).toBeTruthy();
-      expect(authFlow.authorizationResponse.state).toBeTruthy();
-      expect(authFlow.authState.isAuthorizationComplete).toBe(true);
+    describe("Authorization Request", () => {
+      it("makeAuthRequest should work", async () => {
+        await authFlow.makeAuthRequest();
+        expect(authFlow.authorizationResponse).toBeTruthy();
+        expect(authFlow.authorizationResponse.code).toBeTruthy();
+        expect(authFlow.authorizationResponse.state).toBeTruthy();
+        expect(authFlow.authState.isAuthorizationComplete).toBe(true);
+      });
+    });
+
+    describe("Token Request", () => {
+      it("makeTokenRequest should work", async () => {
+        await authFlow.makeTokenRequest();
+        expect(authFlow.tokenResponse).toBeTruthy();
+        expect(authFlow.tokenResponse.refreshToken).toBeTruthy();
+        expect(authFlow.tokenResponse.accessToken).toBeTruthy();
+        expect(authFlow.tokenResponse.idToken).toBeTruthy();
+        expect(authFlow.tokenResponse.scope).toBe(scope);
+        expect(authFlow.authState.isAuthorizationComplete).toBe(true);
+      });
+
+      it("refreshAccessToken should work when access_token is valid", async () => {
+        await authFlow.refreshAccessToken();
+        expect(authFlow.tokenResponse).toBeTruthy();
+        expect(authFlow.tokenResponse.refreshToken).toBeTruthy();
+        expect(authFlow.tokenResponse.accessToken).toBeTruthy();
+        expect(authFlow.tokenResponse.idToken).toBeTruthy();
+        expect(authFlow.tokenResponse.scope).toBe(scope);
+        expect(authFlow.authState.isAuthorizationComplete).toBe(true);
+      });
+
+      it("refreshAccessToken should work when access_token is not valid", async () => {
+        authFlow.tokenResponse.expiresIn = 1;
+        await authFlow.refreshAccessToken();
+        expect(authFlow.tokenResponse).toBeTruthy();
+        expect(authFlow.tokenResponse.refreshToken).toBeTruthy();
+        expect(authFlow.tokenResponse.accessToken).toBeTruthy();
+        expect(authFlow.tokenResponse.idToken).toBeTruthy();
+        expect(authFlow.tokenResponse.scope).toBe(scope);
+        expect(authFlow.authState.isAuthorizationComplete).toBe(true);
+      });
     });
 });
