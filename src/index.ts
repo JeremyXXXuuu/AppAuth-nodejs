05c59fcb1175ecf4157641acd77c65faef148ca8
jeremyxxxuuu <jeremyxu1234@gmail.com>
2023-12-06 15:52:47 +0100

    refactor: removing setupLog

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import { Auth } from "./Auth/auth";
 import { AppAuthClient } from "./appAuthClient";
 import { AuthClient } from "./authClient";
-import { Criticity, Log } from "@orosound/log";
-
-/** Set log file path *********************************************************/
-Log.setupLog({
-  color: true,
-  time: true,
-  verbose: Criticity.DEBUG,
-  path: "./logs",
-});
 
 export { Auth, AppAuthClient, AuthClient };
